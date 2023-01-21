d701bf336863f41badd4ade353e5342e4481a15a
su55y <cesarmilano88@yahoo.com>
2023-01-21 17:01:04 +0200

    logger class

--- logging/custom/main.py
+++ logging/custom/main.py
@@ -1,24 +1,31 @@
 from sys import exit
-from utils.custom_logger import logger 
+
+# from utils.custom_logger import logger
+from utils.custom_logger import CustomLogger
+
+log = CustomLogger(__name__, 0)
+
 
 def div(x, y):
     if not all(isinstance(arg, (int, float)) for arg in [x, y]):
-        logger.warning("x or y are not number, now something is going to happen")
+        log.warning("x or y are not number, now something is going to happen")
 
-    logger.debug(f"try {x} / {y}")
+    log.debug(f"try {x} / {y}")
     try:
         print(f"{x} / {y} = {x / y}")
     except TypeError as e:
-        logger.error(f"can't divide {x} by {y}: {e}")
+        log.error(f"can't divide {x} by {y}: {e}")
     except ZeroDivisionError as e:
-        logger.critical(f"can't divide {x} by {y}: {e}")
+        log.critical(f"can't divide {x} by {y}: {e}")
         exit(1)
 
-def main(): 
+
+def main():
     div(0, "\U0001F43C")
     div(1, 0)
-    logger.info("done")
+    log.info("done")
     exit(0)
+
 
 if __name__ == "__main__":
     main()
--- logging/custom/utils/custom_logger.py
+++ logging/custom/utils/custom_logger.py
@@ -1,5 +1,21 @@
 import logging
 
+
+class CustomLogger(logging.Logger):
+    pass
+
+    def __init__(self, name: str, level) -> None:
+        super().__init__(name, level)
+        self.setFormatter()
+        self.info("custom logger created")
+
+    def setFormatter(self):
+        ch = logging.StreamHandler()
+        ch.setLevel(self.level)
+        ch.setFormatter(CustomFormatter())
+        self.addHandler(ch)
+
+
 class CustomFormatter(logging.Formatter):
     pink_bold = "\x1b[38;5;204;1m"
     pink_underline_bold = "\x1b[38;5;204;4;1m"
@@ -13,7 +29,9 @@ class CustomFormatter(logging.Formatter):
     prefix = "%(asctime)s [%(levelname)s]: "
     message_file = "%(message)s (%(filename)s:%(lineno)d)"
     message_file_func = "%(message)s (%(filename)s:%(funcName)s:%(lineno)d)"
-    debug_msg_fmt = f"%(message)s{reset} \x1b[38;5;197;1;3m(%(filename)s:%(name)s:%(funcName)s:%(lineno)d)"
+    debug_msg_fmt = (
+        f"%(message)s{reset} \x1b[38;5;197;1;3m(%(filename)s:%(funcName)s:%(lineno)d)"
+    )
 
     # docs.python.org/3/library/logging.html#logrecord-attributes
     FORMATS = {
@@ -21,22 +39,10 @@ class CustomFormatter(logging.Formatter):
         logging.INFO: f"{white_bold}{prefix}{white_bold_italic}%(message)s{reset}",
         logging.WARNING: f"{yellow_bold}{prefix}{yellow_bold_italic}{message_file}{reset}",
         logging.ERROR: f"{red_bold}{prefix}{message_file_func}{reset}",
-        logging.CRITICAL: f"{red_bg_bold}{prefix}%(message)s (%(filename)s:%(name)s:%(funcName)s:%(lineno)d){reset}",
+        logging.CRITICAL: f"{red_bg_bold}{prefix}%(message)s (%(filename)s:%(funcName)s:%(lineno)d){reset}",
     }
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt, datefmt="%H:%M:%S %d/%m/%y")
         return formatter.format(record)
-
-# create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG) # 🤔
-ch.setFormatter(CustomFormatter())
-
-logger.addHandler(ch)
-logger.info("custom logger formats created")
