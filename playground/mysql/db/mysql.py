d764c5b4ecf81199f62e1b4c8e021cfe7c2da378
su55y <cesarmilano88@yahoo.com>
2023-02-06 18:22:02 +0200

    vanilla mysql driver pain

---
+++ playground/mysql/db/mysql.py
@@ -0,0 +1,125 @@
+from mysql.connector import connect, Error
+from typing import Any, List, Tuple
+from logging import info
+
+
+def init_db(
+    host: str, port: str, user: str, password: str, database: str, force=False
+) -> Error | None:
+    try:
+        with connect(host=host, port=port, user=user, password=password) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    f"CREATE DATABASE {'IF NOT EXISTS' if not force else ''} {database}"
+                )
+    except Error as e:
+        return e
+
+
+class MyDB:
+    def __init__(self, host: str, port: str, user: str, password: str, database: str):
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+        self._database = database
+
+    def _open_conn(self):
+        return connect(
+            host=self._host,
+            port=self._port,
+            user=self._user,
+            password=self._password,
+            database=self._database,
+        )
+
+    def create_table(
+        self, tb_name: str, fields: List[str], force=False
+    ) -> Error | None:
+        query = f"CREATE TABLE {'IF NOT EXISTS' if not force else ''} {tb_name} ({', '.join(fields)})"
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query)
+        except Error as e:
+            return e
+
+    def select(
+        self, tb_name: str, rows=None, where=None, limit=None
+    ) -> Tuple[Any, Error | None]:
+        rows_str = "*" if not rows else ", ".join(rows)
+        query = f"SELECT {rows_str} FROM {tb_name}{'' if not where else ' WHERE '+where}{'' if not limit else ' LIMIT '+limit}"
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query)
+                    return cursor.fetchall(), None
+        except Error as e:
+            return None, e
+
+    def insert_row(
+        self, tb_name: str, rows: List[str], vals: Tuple[str, str]
+    ) -> Error | None:
+        rows_str = ", ".join(rows)
+        vals_str = ", ".join(list(["%s" for _ in range(len(rows))]))
+        query = f"INSERT INTO {tb_name} ({rows_str}) VALUES({vals_str})"
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query, vals)
+                    conn.commit()
+        except Error as e:
+            return e
+
+    def insert(
+        self,
+        tb_name: str,
+        rows: List[str],
+        vals: List[Tuple[str, str] | Tuple[str, str]],
+    ) -> Tuple[int, Error | None]:
+        rows_str = ", ".join(rows)
+        vals_str = ", ".join(list(["%s" for _ in range(len(rows))]))
+        query = f"INSERT INTO {tb_name} ({rows_str}) VALUES({vals_str})"
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.executemany(query, vals)
+                    conn.commit()
+                    return cursor.rowcount, None
+        except Error as e:
+            return 0, e
+
+    def update(
+        self, tb_name: str, rows: List[str], where: List[str], vals: List[str]
+    ) -> Tuple[int, Error | None]:
+        rows_str = ", ".join(list(map(lambda r: f"{r}=%s", rows)))
+        where_str = " and ".join(list(map(lambda w: f"{w}=%s", where)))
+        query = f"UPDATE {tb_name} SET {rows_str} WHERE {where_str}".strip()
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query, vals)
+                    conn.commit()
+                    return cursor.rowcount, None
+        except Error as e:
+            return 0, e
+
+    def delete(
+        self, tb_name: str, where: List[str], vals: List[str]
+    ) -> Tuple[int, Error | None]:
+        where_str = " and ".join(list(map(lambda w: f"{w}=%s", where)))
+        query = f"DELETE FROM {tb_name} WHERE {where_str}".strip()
+        info(f"try query: '{query}'")
+        try:
+            with self._open_conn() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query, vals)
+                    conn.commit()
+                    return cursor.rowcount, None
+        except Error as e:
+            return 0, e
---
+++ playground/mysql/main.py
@@ -0,0 +1,107 @@
+from config import db_config
+from db.mysql import MyDB, init_db
+from logging import INFO, basicConfig, error, info
+
+
+def die(e: str):
+    error(e)
+    exit(1)
+
+
+def main():
+    basicConfig(
+        level=INFO,
+        format="\x1b[38;5;44m%(asctime)s [%(levelname)s]:\x1b[0m %(message)s",
+        datefmt="%H:%M:%S %d/%m/%y",
+    )
+    if (err := init_db(**db_config)) and err:
+        die(f"DB_CREATE_ERROR: {repr(err)}")
+    my_db = MyDB(**db_config)
+
+    tb_countries = "tb_countries"
+    row_id = "id"
+    row_country = "country"
+    row_capital = "capital"
+
+    if (
+        err := my_db.create_table(
+            tb_name=tb_countries,
+            fields=list(
+                [
+                    f"{row_id} INT NOT NULL AUTO_INCREMENT",
+                    f"{row_country} VARCHAR(255) NOT NULL",
+                    f"{row_capital} VARCHAR(255) NOT NULL",
+                    f"PRIMARY KEY ({row_id})",
+                ]
+            ),
+        )
+    ) and err:
+        die(f"CREATE TABLE ERROR: {repr(err)}")
+
+    if (
+        insert_row_err := my_db.insert_row(
+            tb_name=tb_countries,
+            rows=[row_country, row_capital],
+            vals=("Ukraine", "Kyiv"),
+        )
+    ) and insert_row_err:
+        die(f"INSERT_ROW_ERROR: {repr(insert_row_err)}")
+
+    insert_count, insert_err = my_db.insert(
+        tb_name=tb_countries,
+        rows=[row_country, row_capital],
+        vals=[("United Kingdom", "London"), ("Poland", "Warsaw"), ("India", "Delhi")],
+    )
+    if insert_err:
+        error(f"INSERT_ERROR: {repr(insert_err)}")
+    else:
+        info(f"{insert_count} rows inserted")
+
+    countries, select_err = my_db.select(tb_name=tb_countries)
+    if select_err:
+        error(f"SELECT_ERROR: {repr(select_err)}")
+    else:
+        for c in countries:
+            print(c)
+
+    update_count, update_error = my_db.update(
+        tb_name=tb_countries,
+        rows=[row_capital],
+        vals=["New-Delhi", "India"],
+        where=[row_country],
+    )
+    if update_error:
+        error(f"UPDATE_ERROR: {repr(update_error)}")
+    else:
+        info(f"{update_count} rows updated")
+
+    country, select_row_err = my_db.select(
+        tb_name=tb_countries,
+        rows=[row_country, row_capital],
+        where=f"{row_country} = 'India'",
+    )
+    if select_row_err:
+        error(f"SELECT_ROW_ERROR: {repr(select_row_err)}")
+    else:
+        for c in country:
+            print(c)
+
+    delete_count, delete_error = my_db.delete(
+        tb_name=tb_countries, where=[row_country], vals=["India"]
+    )
+    if delete_error:
+        error(f"DELETE_ERROR {repr(delete_error)}")
+    else:
+        info(f"{delete_count} rows deleted")
+
+    countries2, select_err2 = my_db.select(tb_name=tb_countries)
+    if select_err2:
+        error(f"SELECT_ERROR: {repr(select_err)}")
+    else:
+        print("countries list:")
+        for c in countries2:
+            print(c)
+
+
+if __name__ == "__main__":
+    main()
