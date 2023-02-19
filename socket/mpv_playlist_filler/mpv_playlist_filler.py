b9b3606adfa27da67ab1445ff2522272020077c9
su55y <cesarmilano88@yahoo.com>
2023-02-19 14:50:03 +0200

    refinement of the concept

--- socket/mpv_playlist_filler/mpv_playlist_filler.py
+++ socket/mpv_playlist_filler/mpv_playlist_filler.py
@@ -5,22 +5,25 @@ import logging
 import socket
 import json
 import re
-from typing import Dict
+import time
+from os import path
+from typing import Dict, List
+from sys import argv
 
 log: logging.Logger
+silence = "-s" in argv[1:]
 
 LOG_FMT = "[%(asctime)-.19s %(levelname)-.4s] %(message)s (%(filename)s:%(funcName)s:%(lineno)d)"
 SOCK_FILE = "/tmp/mpv.sock"
-MPV_LOG_FILE = "/tmp/mpv_manager.log"
-APP_LOG_FILE = "/tmp/mpv_manager_app.log"
+APP_LOG_FILE = "/tmp/mpv_playlist_filler.log"
+APPEND_CMD = b"""{"command": ["loadfile", "%s", "append-play"]}\n"""
 NOTIFY_SEND_CMD = ["notify-send", "-i", "mpv", "-a", "mpv"]
 MPV_CMD = [
     "mpv",
     "--idle",
     f"--input-ipc-server={SOCK_FILE}",
     "--no-terminal",
-    "--msg-level=all=info,ao/alsa=error",
-    f"--log-file={MPV_LOG_FILE}",
+    "--really-quiet",
 ]
 
 
@@ -28,9 +31,13 @@ def init_logger():
     global log
     log = logging.getLogger("gloabal_logger")
     log.setLevel(logging.INFO)
-    stream_handler = logging.FileHandler(APP_LOG_FILE, mode="a")
-    stream_handler.setFormatter(logging.Formatter(LOG_FMT))
-    log.addHandler(stream_handler)
+    fh = logging.FileHandler(APP_LOG_FILE, mode="a")
+    fh.setFormatter(logging.Formatter(LOG_FMT))
+    log.addHandler(fh)
+    if not silence:
+        sh = logging.StreamHandler()
+        sh.setFormatter(logging.Formatter(LOG_FMT))
+        log.addHandler(sh)
 
 
 def mpv_exists():
@@ -44,12 +51,20 @@ def mpv_exists():
 
 
 def start_mpv():
-    log.info(f"starting process with output to {MPV_LOG_FILE}")
+    log.info(f"starting mpv")
     try:
         p = sp.Popen(MPV_CMD)
         log.info(p.args)
     except Exception as e:
         log.error(repr(e))
+
+    repeat = 0
+    while not path.exists(SOCK_FILE) or repeat < 10:
+        time.sleep(0.1)
+        repeat += 1
+    if not path.exists(SOCK_FILE):
+        log.error(f"socket file {SOCK_FILE} not found")
+        exit(1)
 
 
 def notify_send(msg: str):
@@ -60,20 +75,19 @@ def notify_send(msg: str):
         print(repr(e))
 
 
-def send_cmd(cmd: bytes) -> Dict | None:
-    data = b""
-    json_data = None
+def send_cmd(cmd: bytes) -> List[Dict] | None:
+    log.info(f"try cmd: {cmd}")
     try:
         with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
             s.connect(SOCK_FILE)
             s.send(cmd)
             data = s.recv(2048)
-            json_data = dict(json.loads(data))
+            log.info(f"resp: {data}")
+            data = data.decode("utf-8", "ignore").encode("utf-8")
+            return json.loads(data.split(b"\n").pop(0))
     except Exception as e:
         log.error(repr(e))
         return None
-
-    return json_data
 
 
 def get_cb() -> str | None:
@@ -85,17 +99,8 @@ def get_cb() -> str | None:
         return out
 
 
-cmd_get_playlist = lambda: b"""{"command": ["get_property", "playlist"]}\n"""
-cmd_append_vid = (
-    lambda url: """{{"command": ["loadfile", "{url}", "append-play"]}}\n""".format(
-        url=url
-    ).encode()
-)
-
-
 def main():
     init_logger()
-    log.info("start")
 
     if not mpv_exists():
         start_mpv()
@@ -104,15 +109,15 @@ def main():
         r".*youtube\.com\/watch\?v=([\w\d_\-]{11})|.*youtu\.be\/([\w\d_\-]{11})|.*twitch\.tv\/videos\/(\d{10})$",
         url,
     ):
-        resp = send_cmd(cmd_append_vid(url))
-        if not resp:
-            log.error("empty socket response")
-        else:
+        if (resp := send_cmd(APPEND_CMD % url.encode())) and isinstance(resp, Dict):
             match resp.get("error"):
                 case "success":
                     notify_send(f"appended {url}")
                 case _:
                     notify_send(f"some error: {resp.get('error')}")
+        else:
+            notify_send(f"invalid cmd response")
+            log.error(f"invalid cmd response ({resp})")
 
 
 if __name__ == "__main__":
