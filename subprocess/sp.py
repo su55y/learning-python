719279a0b8dc737e7d81aa53f35bf93f7d78617a
su55y <cesarmilano88@yahoo.com>
2023-02-20 17:10:23 +0200

    async example

--- subprocess/sp.py
+++ subprocess/sp.py
@@ -1,5 +1,7 @@
 from os.path import dirname, exists
 import subprocess as sp
+import asyncio
+import sys
 
 
 EXE = f"{dirname(__file__)}/print_dates.sh"
@@ -10,14 +12,28 @@ def getoutput_with_pipe() -> str:
 
 
 def sp_run() -> int:
-    return sp.run([EXE]).returncode
+    return sp.run(EXE).returncode
 
 
 def popen_wait_check_output() -> int:
     proc = sp.Popen(EXE, stdout=sp.PIPE, shell=True)
     out = sp.check_output(["grep", "-oP", "\\d{4}"], stdin=proc.stdout)
-    print(out.decode(), end="")
+    sys.stdout.write(out.decode())
     return proc.wait()
+
+
+async def async_sp() -> int:
+    proc = await asyncio.create_subprocess_exec(
+        EXE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    out, err = await proc.communicate()
+    if out:
+        sys.stdout.write(out.decode())
+    if err:
+        sys.stdout.write(err.decode())
+
+    return 1 if proc.returncode is None else proc.returncode
 
 
 def main():
@@ -33,6 +49,9 @@ def main():
 
     print("\n# getoutput()")
     print(getoutput_with_pipe())
+
+    print("\n# async_sp()")
+    print(f"exit with code: {asyncio.run(async_sp())}")
 
 
 if __name__ == "__main__":
