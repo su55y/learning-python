69bb0cc9a30b02573042d93f3b0da8a6e9d625dd
su55y <cesarmilano88@yahoo.com>
2023-03-01 17:13:05 +0200

    tests

--- third_party_packages/pydantic_example/models/http_models.py
+++ third_party_packages/pydantic_example/models/http_models.py
@@ -1,27 +1,30 @@
-from datetime import datetime
 from pydantic import BaseModel, validator
 from pydantic.types import OptionalDate
 
-from enum import Enum, auto
-from http import HTTPStatus
+from datetime import datetime
+from enum import Enum
 from typing import Dict, Optional
 from urllib.parse import urlparse
 
 from .consts import HTTP_STATUS_CODES
 
 
-class HTTPMethod(Enum):
-    GET = auto()
-    POST = auto()
-    PUT = auto()
-    DELETE = auto()
+class HTTPMethod(str, Enum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
 
 
 class HTTPResponse(BaseModel):
-    status: HTTPStatus
     body: Optional[bytes] = None
-    headers: Optional[Dict[str, str]] = None
     date: OptionalDate
+    headers: Dict[str, str] = {}
+    status: int
+
+    @validator("headers", pre=True)
+    def check_headers(cls, v):
+        return v or {}
 
     @validator("status")
     def check_status(cls, v):
@@ -29,48 +32,30 @@ class HTTPResponse(BaseModel):
             raise ValueError(f"invalid status code {v}")
         return v
 
-    @validator("headers")
-    def check_headers(cls, d):
-        if not d:
-            return d
-
-        if not isinstance(d, dict):
-            raise ValueError(f"invalid headers type {type(d).__name__}")
-
-        for k, v in d.items():
-            if not isinstance(k, str):
-                raise ValueError(f"invalid header key type {type(k).__name__}")
-            if not isinstance(v, str):
-                raise ValueError(f"invalid header value type {type(v).__name__}")
-
     @classmethod
     def with_date(cls, obj):
-        obj["date"] = obj.get("date") or datetime.now()
+        obj["date"] = obj.get("date") or datetime.now().date()
         return super().parse_obj(obj)
 
 
 class HTTPRequest(BaseModel):
+    body: Optional[bytes] = None
+    headers: Dict[str, str] = {}
     method: HTTPMethod
     path: str
-    headers: Optional[Dict[str, str]] = None
-    body: Optional[bytes] = None
+
+    @validator("headers", pre=True)
+    def check_headers(cls, v):
+        return v or {}
+
+    @validator("method")
+    def check_method(cls, v):
+        if not isinstance(v, HTTPMethod):
+            raise ValueError(f"invalid method {v}")
+        return v
 
     @validator("path")
     def check_path(cls, v):
         if not v or ((url := urlparse(v)) and not url.path):
             raise ValueError(f"invalid path {v}")
         return v
-
-    @validator("headers")
-    def check_headers(cls, d):
-        if not d:
-            return d
-
-        if not isinstance(d, dict):
-            raise ValueError(f"invalid headers type {type(d).__name__}")
-
-        for k, v in d.items():
-            if not isinstance(k, str):
-                raise ValueError(f"invalid header key type {type(k).__name__}")
-            if not isinstance(v, str):
-                raise ValueError(f"invalid header value type {type(v).__name__}")
