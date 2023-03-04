865c134ba559e7d9e54671a740ebda170f809ab2
su55y <cesarmilano88@yahoo.com>
2023-03-04 22:49:04 +0200

    refactoring

--- argparse/complex/format_numbers/main.py
+++ argparse/complex/format_numbers/main.py
@@ -1,82 +1,89 @@
 #!/usr/bin/env -S python3 -u
 
-from textwrap import dedent
-from argparse import ArgumentParser, Namespace
-from typing import Tuple
-from sys import exit, stderr
+import argparse
+import re
+import textwrap
+from typing import Callable, List, Tuple
+import sys
 
 
-def parse_args(help=False) -> Namespace:
-    parser = ArgumentParser(
+def validate_number(input: str):
+    if not re.match(r"(^(0x)?[a-fA-F0-9]+$)|(^\d+$)", input):
+        raise argparse.ArgumentTypeError(f"invalid number '{input}'")
+    return input
+
+
+def parse_args() -> Tuple[argparse.Namespace, Callable]:
+    parser = argparse.ArgumentParser(
         prog="numconverter",
         description="Shows input number in {dec}, {hex}, {oct} and {bin} bases".format(
-            dec="\x1b[1mdec\x1b[0m",  # bold
+            dec="\x1b[1mdec\x1b[0m",
             hex="\x1b[1mhex\x1b[0m",
             oct="\x1b[1moct\x1b[0m",
             bin="\x1b[1mbin\x1b[0m",
         ),
     )
 
-    parser.add_argument("default", help="input number")
     parser.add_argument(
-        "-x", "--hex", action="store_true", default=False, help="accept input as hex"
+        "numbers",
+        nargs="+",
+        type=validate_number,
+        metavar="NUM",
+        help="input numbers",
     )
-    parser.add_argument(
-        "-o", "--oct", action="store_true", default=False, help="accept input as oct"
-    )
-    parser.add_argument(
-        "-b", "--bin", action="store_true", default=False, help="accept input as bin"
-    )
+    parser.add_argument("-x", "--hex", action="store_true", help="accept input as hex")
+    parser.add_argument("-o", "--oct", action="store_true", help="accept input as oct")
+    parser.add_argument("-b", "--bin", action="store_true", help="accept input as bin")
 
-    if help:
-        parser.print_help(stderr)
-
-    return parser.parse_args()
+    return parser.parse_args(), parser.print_help
 
 
-def parse_number(args: Namespace) -> Tuple[int, bool]:
+def parse_numbers(args: argparse.Namespace) -> Tuple[List[int], Exception | None]:
+    if not args.numbers or not isinstance(args.numbers, List):
+        return [], Exception("invalid input numbers")
+
     base = 10
-    res = 0
-    if args.hex:
-        base = 16
-    elif args.oct:
-        base = 8
-    elif args.bin:
-        base = 2
+    match True:
+        case args.hex:
+            base = 16
+        case args.oct:
+            base = 8
+        case args.bin:
+            base = 2
 
     try:
-        res = int(args.default, base)
+        return [int(n, base) for n in args.numbers], None
     except Exception as e:
-        print(f"\x1b[31;1m{repr(e)}\x1b[0m\n")
-        return res, False
-
-    return res, True
+        return [], e
 
 
-def print_number(num: int):
+def print_numbers(nums: list[int]):
     print(
-        dedent(
+        textwrap.dedent(
             """
-            dec: {0}
-            hex: {0:x}
-            bin: {0:b}
-            oct: {0:o}
+            dec: {dec}
+            hex: {hex}
+            bin: {bin}
+            oct: {oct}
         """.format(
-                num
+                dec=", ".join([f"{n}" for n in nums]),
+                hex=", ".join([f"{n:x}" for n in nums]),
+                bin=", ".join([f"{n:b}" for n in nums]),
+                oct=", ".join([f"{n:o}" for n in nums]),
             )
         ).strip()
     )
 
 
 def main():
-    args = parse_args()
-
-    num, ok = parse_number(args)
-    if not ok:
-        parse_args(help=True)
+    args, print_help = parse_args()
+    nums, err = parse_numbers(args)
+    if err:
+        print(f"\x1b[31;1m{repr(err)}\x1b[0m\n")
+        print_help(sys.stdout)
         exit(1)
 
-    print_number(num)
+    print_numbers(nums)
 
 
 if __name__ == "__main__":
