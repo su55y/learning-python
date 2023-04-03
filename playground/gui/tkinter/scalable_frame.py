6fb656bd05a1cab37634133e29eeba5a754d75bc
su55y <cesarmilano88@yahoo.com>
2023-04-04 02:44:04 +0300

    scalable frame example

---
+++ playground/gui/tkinter/scalable_frame.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+
+
+BOARD_LENGTH = 8  # !! max_scale == (screen_height/2)*BOARD_LENGTH
+
+
+class ScalableFrame(tk.Frame):
+    def __init__(self, parent: tk.Tk, height: float, **kwargs):
+        tk.Frame.__init__(self, parent, height=height, **kwargs)
+        self.bind_all("<Button-4>", self.zoom)
+        self.bind_all("<Button-5>", self.zoom)
+        self.max_scale = height * BOARD_LENGTH
+        self.min_scale = height / BOARD_LENGTH
+        self.canvas_size = height
+
+    def zoom(self, event: tk.Event):
+        match event.num:
+            case 4:
+                self.scale = 2
+            case 5:
+                self.scale = 0.5
+            case _:
+                return
+        if (
+            self.max_scale <= self.scale * self.canvas_size
+            or self.min_scale >= self.scale * self.canvas_size
+        ):
+            return
+        if child := self.winfo_children().pop():
+            if isinstance(child, tk.Canvas):
+                self.canvas_size = child.winfo_height() * self.scale
+
+                child.scale("all", 0, 0, self.scale, self.scale)
+                child.place(
+                    relx=0.5,
+                    rely=0.5,
+                    width=self.canvas_size,
+                    height=self.canvas_size,
+                )
+
+
+def chessboard_canvas(parent: ScalableFrame, **kwargs):
+    canvas = tk.Canvas(parent, **kwargs)
+    if height := kwargs.get("height"):
+        color_switch = False
+        col = 0
+        rect_size = height / BOARD_LENGTH
+        for x, y in [
+            (x, y)
+            for x in range(0, height, int(rect_size))
+            for y in range(0, height, int(rect_size))
+        ]:
+            if col != x:
+                color_switch = not color_switch
+            color_switch = not color_switch
+            col = x
+
+            canvas.create_rectangle(
+                x,
+                y,
+                x + rect_size,
+                y + rect_size,
+                fill=["#000", "#fff"][color_switch],
+                width=0,
+            )
+    return canvas
+
+
+if __name__ == "__main__":
+    window = tk.Tk(className="floating")
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    window_size = int(screen_height * 0.5)
+    window_x = int((screen_width - window_size) / 2)
+    window_y = int((screen_height - window_size) / 2)
+    window.geometry(f"{window_size}x{window_size}+{window_x}+{window_y}")
+
+    frame = ScalableFrame(
+        window,
+        width=window_size,
+        height=window_size,
+        highlightthickness=0,
+    )
+    frame.pack(expand=True)
+
+    chessboard_canvas(
+        frame,
+        width=window_size,
+        height=window_size,
+        borderwidth=0,
+        highlightthickness=0,
+    ).place(relx=0.5, rely=0.5, anchor="center")
+
+    exit(window.mainloop())
