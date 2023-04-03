dada6561e562eed3cc3778021bde3b67580ab433
su55y <cesarmilano88@yahoo.com>
2023-04-03 17:26:47 +0300

    both directions 🙃

--- gui/tkinter/pb.py
+++ gui/tkinter/pb.py
@@ -1,12 +1,11 @@
-from collections.abc import Callable
+from time import sleep
+
 from tkinter import Tk, Button, Label, Frame, StringVar
 from tkinter.ttk import Progressbar
-from time import sleep
-from typing import Any
 
 
-def with_hide(callback: Callable[..., Any]) -> Callable[..., Any]:
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
+def with_hide(callback):
+    def wrapper(*args, **kwargs):
         global start_button
         start_button.pack_forget()
         callback(*args, **kwargs)
@@ -18,26 +17,33 @@ def with_hide(callback: Callable[..., Any]) -> Callable[..., Any]:
 @with_hide
 def start():
     global progress_bar, percent, window
-    progress_bar.configure(value=0)
+    match progress_bar["value"]:
+        case 101:
+            direction = -1
+            condition = lambda v: v >= 0
+        case _:
+            direction = 1
+            condition = lambda v: v <= 100
 
-    while progress_bar["value"] <= 100:
+    while condition(progress_bar["value"]):
         percent.set(f"{progress_bar['value']}%")
-        progress_bar["value"] += 1
+        progress_bar["value"] += direction
         window.update_idletasks()
         sleep(0.01)
 
 
-window = Tk()
-frame = Frame(window, padx=25, pady=25)
-frame.pack()
+if __name__ == "__main__":
+    window = Tk()
+    frame = Frame(window, padx=25, pady=25)
+    frame.pack()
 
-progress_bar = Progressbar(frame, length=300, value=0)
-progress_bar.pack(pady=10)
+    progress_bar = Progressbar(frame, length=300, value=0)
+    progress_bar.pack(pady=10)
 
-percent = StringVar()
-percent_label = Label(frame, textvariable=percent).pack()
+    percent = StringVar()
+    percent_label = Label(frame, textvariable=percent).pack()
 
-start_button = Button(frame, text="start", command=start)
-start_button.pack()
+    start_button = Button(frame, text="start", command=start)
+    start_button.pack()
 
-window.mainloop()
+    window.mainloop()
