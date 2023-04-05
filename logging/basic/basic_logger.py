e348089d87821512e8c6f0f7bb3f60a2cda4618f
su55y <cesarmilano88@yahoo.com>
2023-04-05 21:28:44 +0300

    upd basic logger example

--- logging/basic/basic_logger.py
+++ logging/basic/basic_logger.py
@@ -1,15 +1,30 @@
+from enum import Enum, auto
 import logging
 
-logging.basicConfig(
-  level=logging.INFO,
-  # filename="log",
-  # filemode="w",
-  format="\x1b[38;5;44m%(asctime)s [%(levelname)s]:\x1b[0m %(message)s",
-  datefmt="%H:%M:%S %d/%m/%y"
-)
 
-logging.debug("debug log")
-logging.info("info log")
-logging.warning("warning log")
-logging.error("error log")
-logging.critical("critical log")
+class HandlerType(Enum):
+    Stream = auto()
+    File = auto()
+
+
+def create_logger(**kwargs):
+    logger = logging.getLogger(kwargs.get("name", __name__))
+    if level := kwargs.get("level"):
+        logger.setLevel(level)
+
+    match kwargs.get("handler_type", HandlerType.Stream):
+        case HandlerType.Stream:
+            handler = logging.StreamHandler()
+        case HandlerType.File:
+            path = kwargs.get("path")
+            if not path:
+                path = f"{__name__}.log"
+            handler = logging.FileHandler(path)
+        case _:
+            raise NotImplemented(f"this handler type not implemented yet")
+
+    if format := kwargs.get("format"):
+        handler.setFormatter(logging.Formatter(format))
+
+    logger.addHandler(handler)
+    return logger
