1a52b8521ffde6f05223f2230a5762e870d6d1fd
su55y <cesarmilano88@yahoo.com>
2023-04-10 22:19:07 +0300

    refactoring

--- http/sync/sync.py
+++ http/sync/sync.py
@@ -1,15 +1,30 @@
 from html.parser import HTMLParser
-from logging import info, error, basicConfig, INFO
-from os import path, mkdir
+
+# from logging import info, error, basicConfig, INFO
+import logging
+from pathlib import Path
 from re import match
 from textwrap import wrap
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 from urllib.request import urlopen
 
 PROJECT_EULER_URL = "https://projecteuler.net/minimal=%d"
 RESULTS_URL = "https://raw.githubusercontent.com/zengin/project-euler-solutions/master/results.txt"
 
 PROBLEMS_DIR_NAME = "problems"
+LOG_FMT = "[\x1b[38;5;44m%(asctime)s %(levelname)s\x1b[0m] %(message)s"
+
+
+def init_logger():
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter(LOG_FMT, "%H:%M:%S %d/%m/%y"))
+    log.addHandler(sh)
+    return log
+
+
+log = init_logger()
 
 
 class EulerMinimalParser(HTMLParser):
@@ -38,71 +53,55 @@ def parse_results(raw_results: str) -> Dict[int, int]:
 
 
 def get_results() -> Dict[int, int] | None:
-    raw_results, ok = make_req(RESULTS_URL, timeout=300)
-    if not ok:
-        return None
-    return parse_results(raw_results)
+    if raw_results := make_req(RESULTS_URL, timeout=300):
+        return parse_results(raw_results)
 
 
-def init_dir() -> bool:
-    ABS_DIR_PATH = path.abspath(f"./{PROBLEMS_DIR_NAME}")
-    if not path.exists(ABS_DIR_PATH):
-        try:
-            mkdir(ABS_DIR_PATH)
-        except Exception as e:
-            error(repr(e))
-            return False
-        else:
-            info(f"{ABS_DIR_PATH} created")
-    elif path.isdir(ABS_DIR_PATH):
-        info(f"{ABS_DIR_PATH} already exists")
-
-    return True
-
-
-def make_req(URL, timeout=1000) -> Tuple[str, bool]:
+def init_dir() -> Path | None:
+    dir_path = Path(PROBLEMS_DIR_NAME)
     try:
-        with urlopen(URL, timeout=timeout) as resp:
-            info(f"GET {resp.status} {resp.msg} {URL}")
-            if resp.status == 200:
-                return resp.read().decode().strip(), True
-    except Exception as e:
-        error(repr(e))
+        if not dir_path.exists():
+            dir_path.mkdir()
+    except:
+        log.error(f"can't make dir by path {dir_path.absolute()}")
+    else:
+        return dir_path.absolute()
 
-    return "", False
+
+def make_req(url: str, timeout=1000) -> str | None:
+    try:
+        with urlopen(url, timeout=timeout) as resp:
+            log.info(f"GET {resp.status} {resp.msg} {url}")
+            if resp.status == 200:
+                return resp.read().decode().strip()
+    except Exception as e:
+        log.error(repr(e))
+
+
+def write_problem(file_path, content):
+    try:
+        with open(file_path, "w") as file:
+            file.write(content)
+    except Exception as e:
+        log.error(repr(e))
 
 
 def main():
-    basicConfig(
-        level=INFO,
-        format="\x1b[38;5;44m%(asctime)s [%(levelname)s]:\x1b[0m %(message)s",
-        datefmt="%H:%M:%S %d/%m/%y",
-    )
-    if not init_dir():
+    if not (dir_path := init_dir()):
         exit(1)
-    results = get_results()
     euler_parser = EulerMinimalParser()
-    problems_list: List[Tuple[int, str, int | None]] = list()
+    results = get_results() or {}
 
     for i in range(1, 51):
-        raw_problem, ok = make_req(PROJECT_EULER_URL % i)
-        if not ok:
+        if not (raw_problem := make_req(PROJECT_EULER_URL % i)):
             continue
-
         euler_parser.feed(raw_problem)
-        result = None
-        if results and i in results.keys():
-            result = results.get(i)
-
-        problems_list.append((i, euler_parser.get_problem(), result))
-
-    for (index, text, result) in problems_list:
-        try:
-            with open(f"{PROBLEMS_DIR_NAME}/problem{index}", "w") as file:
-                result = "" if not result else f"\nexpected result: {result}\n"
-                file.write(f"Problem #{index}\n{text}\n{result}")
-        except Exception as e:
-            error(repr(e))
+        result_value = results.get(i)
+        result = "" if not result_value else f"\nexpected result: {result_value}\n"
+        write_problem(
+            dir_path.joinpath(f"problem{i}.txt"),
+            f"Problem #{i}\n{euler_parser.get_problem()}\n{result}",
+        )
 
 
 if __name__ == "__main__":
