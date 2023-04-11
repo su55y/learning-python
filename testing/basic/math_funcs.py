b5e8005a55e8b54710d1b72d25b09ab2f2a5a020
su55y <cesarmilano88@yahoo.com>
2023-04-11 23:00:50 +0300

    simplified example

--- testing/basic/math_funcs.py
+++ testing/basic/math_funcs.py
@@ -1,3 +1,3 @@
 from functools import reduce
 
-get_sum = lambda nums: reduce(lambda next, prev: next + prev, nums)
+count_sum = lambda nums: reduce(lambda next, prev: next + prev, nums)
--- testing/basic/math_funcs_tests.py
+++ testing/basic/math_funcs_tests.py
@@ -1,16 +1,17 @@
 import unittest
-from math_funcs import sum
+
+from math_funcs import count_sum
 
 
-class TestSum(unittest.TestCase):
+class TestMathFuncs(unittest.TestCase):
     def test_sum_list(self):
-        self.assertEqual(sum.get_sum([1, 2, 3]), 6, "Should be 6")
+        self.assertEqual(count_sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
-        self.assertEqual(sum.get_sum((1, 2, 3)), 6, "Should be 6")
+        self.assertEqual(count_sum((1, 2, 3)), 6, "Should be 6")
 
-    def test_sum_dict(self):
-        self.assertEqual(sum.get_sum({1, 2, 3}), 6, "Should be 6")
+    def test_sum_set(self):
+        self.assertEqual(count_sum({1, 2, 3}), 6, "Should be 6")
 
 
 if __name__ == "__main__":
