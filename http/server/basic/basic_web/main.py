39b6aef2d26a39b1ec49b9b08a7fe6c08e69d96b
su55y <cesarmilano88@yahoo.com>
2023-04-15 19:56:48 +0300

    args + icon

--- http/server/basic/basic_web/main.py
+++ http/server/basic/basic_web/main.py
@@ -1,9 +1,13 @@
+import argparse
+from http import HTTPStatus
 import http.server
+import gzip
 import os
 import sys
 import time
 import threading
 
+HOST = "localhost"
 PORT = 8000
 STATIC_DIR = os.path.join(os.getcwd(), "static")
 
@@ -24,7 +28,7 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
         if self.command == "GET":
             self.routing().get(self.path, self.handle_not_found)()
         else:
-            self.send_error(405)
+            self.send_error(HTTPStatus.METHOD_NOT_ALLOWED)
 
     def filter_path(self, path):
         match path:
@@ -32,6 +36,8 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
                 return "index.html"
             case "/about":
                 return "pages/about.html"
+            case "/favicon.ico":
+                return "images/favicon.ico"
             case _:
                 return ""
 
@@ -41,11 +47,24 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
             "/index.html": self.handle_page,
             "/about": self.handle_page,
             "/time": self.handle_get_time,
+            "/favicon.ico": self.handle_icon,
         }
+
+    def handle_icon(self):
+        if image := get_static_file(self.filter_path(self.path)):
+            compressed = gzip.compress(image)
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-Type", "image/x-icon")
+            self.send_header("Content-Encoding", "gzip")
+            self.send_header("Content-Length", f"{len(compressed)}")
+            self.end_headers()
+            self.wfile.write(compressed)
+        else:
+            self.handle_not_found()
 
     def handle_page(self):
         if page := get_static_file(self.filter_path(self.path)):
-            self.send_response(200)
+            self.send_response(HTTPStatus.OK)
             self.send_header("Content-Type", "text/html; charset=utf-8")
             self.send_header("Content-Length", f"{len(page)}")
             self.end_headers()
@@ -70,14 +89,14 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def handle_get_time(self):
         time_str = time.strftime("%H:%M:%S").encode()
-        self.send_response(200)
+        self.send_response(HTTPStatus.OK)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Content-Length", f"{len(time_str)}")
         self.end_headers()
         self.wfile.write(time_str)
 
     def handle_not_found(self):
-        self.send_error(404)
+        self.send_error(HTTPStatus.NOT_FOUND)
 
     def send_error(self, code, message=None):
         self.send_response(code, message)
@@ -89,14 +108,17 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
         self.server.socket.close()
 
 
-if __name__ == "__main__":
-    if len(sys.argv[1:]) == 1 and (port := sys.argv[1]):
-        try:
-            PORT = int(port)
-        except:
-            pass
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--addr", default=HOST)
+    parser.add_argument("-p", "--port", default=PORT)
+    return parser.parse_args()
 
-    with http.server.HTTPServer(("", PORT), BasicRequestHandler) as httpd:
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    with http.server.HTTPServer((args.addr, args.port), BasicRequestHandler) as httpd:
         shutdown = threading.Thread(target=httpd.shutdown, daemon=True)
         try:
             host, port = httpd.server_address
