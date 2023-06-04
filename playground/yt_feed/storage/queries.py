d7533cefc3344fabf36bd0dde727ff32de3959ea
su55y <cesarmilano88@yahoo.com>
2023-06-04 19:24:11 +0300

    storage update

--- playground/yt_feed/storage/queries.py
+++ playground/yt_feed/storage/queries.py
@@ -1,8 +1,15 @@
 QUERIES = [
+    "PRAGMA foreign_keys = ON;",
+    """
+    CREATE TABLE if not exists tb_feeds(
+        channel_id VARCHAR(24) NOT NULL PRIMARY KEY,
+        title VARCHAR NOT NULL,
+    );""",
     """
     CREATE TABLE IF NOT EXISTS tb_entries(
         id VARCHAR(11) NOT NULL PRIMARY KEY,
         title VARCHAR NOT NULL,
-        updated DATETIME NOT NULL
+        updated DATETIME NOT NULL,
+        channel_id VARCHAR(24) NOT NULL REFERENCES tb_feeds(channel_id) ON DELETE CASCADE
     );""",
 ]
--- playground/yt_feed/storage/storage.py
+++ playground/yt_feed/storage/storage.py
@@ -2,9 +2,9 @@ from contextlib import contextmanager
 import logging
 from pathlib import Path
 import sqlite3
-from typing import List
+from typing import List, Optional
 
-from models import Entry
+from models import Feed, Entry
 
 
 class Storage:
@@ -24,16 +24,42 @@ class Storage:
         finally:
             conn.close()
 
-    def add_entries(self, entries: List[Entry]) -> int:
-        if not entries:
+    def fetch_feed(self, channel_id: str) -> Optional[Feed]:
+        with self.get_cursor() as cursor:
+            query = "SELECT channel_id, title FROM tb_feeds WHERE channel_id = ?"
+            self.log.debug(query)
+            if feed_row := cursor.execute(query, channel_id).fetchone():
+                _channel_id, title = feed_row
+                return Feed(
+                    channel_id=_channel_id,
+                    title=title,
+                    entries=self.fetch_feed_entries(channel_id),
+                )
+
+    def fetch_feed_entries(self, channel_id: str) -> List[Entry]:
+        entries: List[Entry] = []
+        with self.get_cursor() as cursor:
+            query = "SELECT id, title, updated FROM tb_entries WHERE channel_id = ?"
+            self.log.debug(query)
+            rows = cursor.execute(query, channel_id).fetchall()
+            for _id, title, updated in rows:
+                entries.append(Entry(id=_id, title=title, updated=updated))
+        return entries
+
+    def add_feed(self, feed: Feed) -> int:
+        with self.get_cursor() as cursor:
+            query = "INSERT INTO tb_feeds (channel_id, title) VALUES (?, ?)"
+            self.log.debug(query)
+            return cursor.execute(query, (feed.channel_id, feed.title)).rowcount
+
+    def add_entries(self, feed: Feed) -> int:
+        if not feed.entries:
             return 0
         with self.get_cursor() as cursor:
-            query = (
-                "INSERT OR IGNORE INTO tb_entries (id, title, updated) VALUES (?, ?, ?)"
-            )
-            self.log.debug(query)
-            cursor.executemany(
-                query,
-                [(entry.id, entry.title, entry.updated) for entry in entries],
-            )
-            return cursor.rowcount
+            query = "INSERT OR IGNORE INTO tb_entries (id, title, updated, channel_id) VALUES (?, ?, ?, ?)"
+            entries = [
+                (entry.id, entry.title, entry.updated, feed.channel_id)
+                for entry in feed.entries
+            ]
+            self.log.debug(f"{query}, entries count: {len(entries)}")
+            return cursor.executemany(query, entries).rowcount
