f1602c70438d003a86157c46ce4b9637c2d4e01a
su55y <cesarmilano88@yahoo.com>
2023-06-10 19:47:00 +0300

    removed kb

--- playground/rofi_modi/launcher.py
+++ playground/rofi_modi/launcher.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from subprocess import run
 
 PANDA = "\U0001F43C"
-ROFI_BASE_CMD = f"rofi -show {PANDA} -modi {PANDA}:%s -no-config -kb-custom-1 Ctrl+s -kb-custom-2 Ctrl+a -kb-move-front 0"
+ROFI_BASE_CMD = f"rofi -show {PANDA} -modi {PANDA}:%s -no-config -kb-custom-1 Ctrl+s"
 
 
 if __name__ == "__main__":
