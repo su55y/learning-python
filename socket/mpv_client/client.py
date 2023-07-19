831f082cbf74a01304cf1f459928577f67e59133
su55y <cesarmilano88@yahoo.com>
2023-07-19 20:03:39 +0300

    update playlist after append

--- socket/mpv_client/client.py
+++ socket/mpv_client/client.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import socket
 import subprocess as sp
 import time
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 
 def init_logger(file: Optional[Path] = None):
@@ -44,6 +44,14 @@ def parse_args() -> argparse.Namespace:
         metavar="PATH",
         help="log file location for debugging purposes",
     )
+    parser.add_argument(
+        "-p",
+        "--playlist-file",
+        default=Path("/tmp/mpv_playlist.json"),
+        type=Path,
+        metavar="PATH",
+        help="playlist state file (%(default)s)",
+    )
     return parser.parse_args()
 
 
@@ -72,20 +80,33 @@ def notify(msg: str) -> None:
         logging.error(e)
 
 
-class MpvClient:
-    def __init__(self, socket_file: Path) -> None:
-        self.socket_file = socket_file
+def fetch_title(url: str) -> str:
+    try:
+        return sp.getoutput("yt-dlp -e %s" % url)
+    except Exception as e:
+        logging.error("can't fetch title for %r: %s" % (url, e))
+        return url
 
-    def playlist(self):
+
+class MpvClient:
+    def __init__(self, socket_file: Path, playlist_file: Path) -> None:
+        self.socket_file = socket_file
+        self.playlist_file = playlist_file
+
+    def playlist(self) -> List[Dict]:
         with connect(self.socket_file) as sock:
             cmd = '{"command": ["get_property", "playlist"]}\n'
             logging.debug(cmd)
             sock.sendall(cmd.encode())
             if not (resp := self._read(sock)):
                 exit("can't read response")
-            print("%r" % resp)
+            if (err := resp.get("error")) != "success":
+                exit(err)
+            if (data := resp.get("data")) is None:
+                exit("data not found in resp: %r" % resp)
+            return data
 
-    def append(self, file: str):
+    def append(self, file: str) -> None:
         with connect(self.socket_file) as sock:
             cmd = '{ "command": ["loadfile", "%s", "append-play"] }\n' % file
             logging.debug("send %r" % cmd)
@@ -93,13 +114,34 @@ class MpvClient:
             if not (resp := self._read(sock)):
                 exit("can't read response")
             elif (err := resp.get("error")) != "success":
-                exit("erdebugoccured: %r" % err)
+                exit(err)
             elif not (data := resp.get("data")):
                 exit("invalid response: %r" % resp)
 
             msg = "entry with id %d just added" % data.get("playlist_entry_id", -1)
             logging.info(msg)
             notify(msg)
+            self.update_playlist()
+
+    def update_playlist(self) -> None:
+        mpv_playlist = self.playlist()
+        playlist_storage = {}
+        if self.playlist_file.exists():
+            with open(self.playlist_file) as f:
+                playlist_storage = json.load(f)
+        for vid in mpv_playlist:
+            if (url := vid.get("filename")) not in playlist_storage.keys():
+                if not url:
+                    logging.error(
+                        "invalid playlist %r, filename missing for %r"
+                        % (mpv_playlist, vid)
+                    )
+                    continue
+                vid["title"] = fetch_title(url)
+                logging.debug("new entry in playlist file: %r" % vid)
+                playlist_storage[url] = vid
+        with open(self.playlist_file, "w") as f:
+            json.dump(playlist_storage, f)
 
     def _read(self, conn: socket.socket) -> Optional[Dict]:
         data = b""
@@ -151,8 +193,9 @@ if __name__ == "__main__":
         exit("%s not found" % args.file)
     if not args.file.is_socket():
         exit("%s is not a socket file" % args.file)
-    client = MpvClient(args.file)
+    client = MpvClient(args.file, args.playlist_file)
     if args.append:
         client.append(args.append)
+    # TODO: print formatted for rofi playlist
     elif args.playlist:
         client.playlist()
