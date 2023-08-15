c3df13c6f31e218935ed1bf487f2d12237b51f56
su55y <cesarmilano88@yahoo.com>
2023-08-15 11:38:24 +0300

    rm unrelated code

--- http/download_eulers/async.py
+++ http/download_eulers/async.py
@@ -1,21 +1,19 @@
 import asyncio
-from typing import List
+from typing import List, Optional
 
 from aiohttp import ClientSession
 
-from utils import *
+from utils import log, ProblemsHandler, PROJECT_EULER_URL, RESULTS_URL
 
 
-async def fetch_problem(session: ClientSession, url: str) -> str | None:
+async def fetch_problem(session: ClientSession, url: str) -> Optional[str]:
     async with session.get(url) as resp:
         log.info(f"{resp.status} {resp.reason} {resp.url}")
-        if resp.status != 200:
-            resp.raise_for_status()
-        else:
+        if resp.status == 200:
             return await resp.text()
 
 
-async def fetch_all_problems(session: ClientSession) -> List:
+async def fetch_all_problems(session: ClientSession) -> List[str]:
     tasks = [
         asyncio.create_task(fetch_problem(session, PROJECT_EULER_URL % i))
         for i in range(1, 51)
@@ -24,24 +22,18 @@ async def fetch_all_problems(session: ClientSession) -> List:
 
 
 async def main():
-    if not (dir_path := init_dir()):
-        exit(1)
-    euler_parser = EulerMinimalParser()
     async with ClientSession() as session:
+        results = ""
         async with session.get(RESULTS_URL) as resp:
             log.info(f"{resp.status} {resp.reason} {resp.url}")
-            results = parse_results(await resp.text()) or {}
-
-        for i, raw_problem in enumerate(await fetch_all_problems(session)):
-            euler_parser.feed(raw_problem)
-            result_value = results.get(i)
-            result = "" if not result_value else f"\nexpected result: {result_value}\n"
-            write_problem(
-                dir_path.joinpath(f"problem{i}.txt"),
-                f"Problem #{i}\n{euler_parser.get_problem()}\n{result}",
-            )
-            pass
+            results = await resp.text()
+        problems = await fetch_all_problems(session)
+        h = ProblemsHandler(raw_results=results, raw_problems=problems)
+        h.write_problems()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(repr(e))
--- http/download_eulers/sync.py
+++ http/download_eulers/sync.py
@@ -1,40 +1,25 @@
-from typing import Dict
 from urllib.request import urlopen
 
-from utils import *
+from utils import log, ProblemsHandler, PROJECT_EULER_URL, RESULTS_URL
 
 
-def get_results() -> Dict[int, int] | None:
-    if raw_results := make_req(RESULTS_URL, timeout=300):
-        return parse_results(raw_results)
-
-
-def make_req(url: str, timeout=1000) -> str | None:
+def fetch(url: str, timeout=1000) -> str:
     try:
         with urlopen(url, timeout=timeout) as resp:
             log.info(f"GET {resp.status} {resp.msg} {url}")
-            if resp.status == 200:
-                return resp.read().decode().strip()
+            if resp.status != 200:
+                raise Exception(f"response status {resp.status}")
+            return resp.read().decode().strip()
     except Exception as e:
-        log.error(repr(e))
+        log.error("can't fetch results: %s" % e)
+        return ""
 
 
 def main():
-    if not (dir_path := init_dir()):
-        exit(1)
-    euler_parser = EulerMinimalParser()
-    results = get_results() or {}
-
-    for i in range(1, 51):
-        if not (raw_problem := make_req(PROJECT_EULER_URL % i)):
-            continue
-        euler_parser.feed(raw_problem)
-        result_value = results.get(i)
-        result = "" if not result_value else f"\nexpected result: {result_value}\n"
-        write_problem(
-            dir_path.joinpath(f"problem{i}.txt"),
-            f"Problem #{i}\n{euler_parser.get_problem()}\n{result}",
-        )
+    results = fetch(RESULTS_URL)
+    problems = [p for i in range(1, 51) if (p := fetch(PROJECT_EULER_URL % i))]
+    h = ProblemsHandler(raw_problems=problems, raw_results=results)
+    h.write_problems()
 
 
 if __name__ == "__main__":
