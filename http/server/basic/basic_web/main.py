58e8d6e9b85786ba5760f9e3b09cab124ff7a8ac
su55y <cesarmilano88@yahoo.com>
2023-08-17 20:31:40 +0300

    refactoring

--- http/server/basic/basic_web/main.py
+++ http/server/basic/basic_web/main.py
@@ -1,39 +1,65 @@
 import argparse
+from collections.abc import Callable
 from http import HTTPStatus
 import http.server
 import json
 import gzip
+import logging as log
 import mimetypes
-import os
+from pathlib import Path
 import socketserver
 import sys
 import time
 import threading
+from typing import Optional
 
 PORT = 8000
-STATIC_DIR = os.path.join(os.getcwd(), "static")
-CACHE = dict()
+STATIC_DIR = Path(__file__).parent.joinpath("static")
+CACHE = {}
+
+log.basicConfig(
+    level=log.DEBUG,
+    format="%(asctime)s %(levelname)s] %(message)s (%(filename)s:%(lineno)d)",
+    datefmt="%H:%M:%S",
+)
 
 
 def get_static_file(path) -> bytes | None:
     try:
-        filepath = os.path.join(STATIC_DIR, path)
-        with open(filepath, "rb") as f:
+        log.debug("opening static: %s" % path)
+        with open(STATIC_DIR.joinpath(path), "rb") as f:
             content = f.read()
             CACHE[path] = content
             return content
     except Exception as e:
-        print(e)
+        log.error("can't get static file: %s" % e)
 
 
 def get_mimetype(path) -> str | None:
-    mimetype, _ = mimetypes.guess_type(os.path.join(STATIC_DIR, path))
+    mimetype, _ = mimetypes.guess_type(STATIC_DIR.joinpath(path))
     return mimetype
 
 
 class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
     server_version = "Basic Web Server"
     protocol_version = "HTTP/1.1"
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.paths = {
+            "/": "index.html",
+            "/about": "pages/about.html",
+            "/favicon.ico": "images/favicon.ico",
+            "/main.js": "scripts/main.js",
+            "/style.css": "css/style.css",
+        }
+        super().__init__(*args, **kwargs)
+
+    def routing(self, path: str) -> Optional[Callable]:
+        match path:
+            case "/time":
+                return self.handle_get_time
+            case _:
+                return lambda: self.handle_file(compress=True)
 
     def do_OPTIONS(self):
         self.handle_request()
@@ -51,36 +77,12 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
         self.handle_request()
 
     def handle_request(self):
+        log.debug("%s %s" % (self.command, self.path))
         if self.command == "GET":
-            self.routing().get(self.path, self.handle_not_found)()
+            (self.routing(self.path) or self.handle_not_found)()
         else:
             self.send_error(HTTPStatus.METHOD_NOT_ALLOWED)
-
-    def filter_path(self, path):
-        match path:
-            case "/":
-                return "index.html"
-            case "/about":
-                return "pages/about.html"
-            case "/favicon.ico":
-                return "images/favicon.ico"
-            case "/main.js":
-                return "scripts/main.js"
-            case "/style.css":
-                return "css/style.css"
-            case _:
-                return ""
-
-    def routing(self):
-        return {
-            "/": self.handle_file,
-            "/index.html": self.handle_file,
-            "/about": self.handle_file,
-            "/time": self.handle_get_time,
-            "/favicon.ico": self.handle_file,
-            "/style.css": self.handle_file,
-            "/main.js": lambda: self.handle_file(compress=False),
-        }
+        self.close_connection = True
 
     def handle_not_found(self):
         self.send_error(HTTPStatus.NOT_FOUND)
@@ -92,21 +94,26 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
     def handle_file(self, compress=True):
-        filepath = self.filter_path(self.path)
-        if file := CACHE.get(filepath, get_static_file(filepath)):
-            self.send_response(HTTPStatus.OK)
-            if mimetype := get_mimetype(filepath):
-                self.send_header("Content-Type", mimetype)
-            if compress:
-                file = gzip.compress(file)
-                self.send_header("Content-Encoding", "gzip")
-                self.send_header("Content-Length", f"{len(file)}")
-            else:
-                self.send_header("Content-Length", f"{len(file)}")
-            self.end_headers()
-            self.wfile.write(file)
-        else:
+        filepath = self.paths.get(self.path)
+        if not filepath:
+            log.debug("filepath for %r not found" % self.path)
             self.handle_not_found()
+            return
+        log.debug("handling file %r by path %r" % (filepath, self.path))
+
+        file = CACHE.get(filepath, get_static_file(filepath))
+        if not file:
+            self.handle_not_found()
+            return
+        self.send_response(HTTPStatus.OK)
+        if mimetype := get_mimetype(filepath):
+            self.send_header("Content-Type", mimetype)
+        if compress:
+            file = gzip.compress(file)
+            self.send_header("Content-Encoding", "gzip")
+        self.send_header("Content-Length", str(len(file)))
+        self.end_headers()
+        self.wfile.write(file)
 
     def handle_get_time(self):
         time_resp = json.dumps({"time": time.strftime("%H:%M:%S")}).encode()
@@ -115,9 +122,6 @@ class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header("Content-Length", f"{len(time_resp)}")
         self.end_headers()
         self.wfile.write(time_resp)
-
-    def shutdown(self):
-        self.server.socket.close()
 
 
 def parse_args():
@@ -138,6 +142,8 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
+    if not STATIC_DIR.exists() or not STATIC_DIR.is_dir():
+        exit(1)
 
     with socketserver.TCPServer(("", args.port), BasicRequestHandler) as httpd:
         shutdown = threading.Thread(target=httpd.shutdown, daemon=True)
