ffe282ce8ebe2f1fb47eca96a9fc65a5f7a31f44
su55y <cesarmilano88@yahoo.com>
2023-08-26 18:26:06 +0300

    simplified basic handler

--- http/server/basic/request_handler/main.py
+++ http/server/basic/request_handler/main.py
@@ -1,5 +1,4 @@
 import http.server
-import signal
 import sys
 import time
 import threading
@@ -10,39 +9,26 @@ PORT = 8000
 class BasicRequestHandler(http.server.BaseHTTPRequestHandler):
     server_version = "Basic Server"
 
-    def handle_get_time(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(time.strftime("%H:%M:%S").encode())
-
-    def handle_not_found(self):
-        self.send_error(404)
-
-    def routing(self):
-        return {
-            "/time": self.handle_get_time,
-        }
-
     def do_GET(self):
-        self.routing().get(self.path, self.handle_not_found)()
+        (self.handle_hello_world if self.path == "/" else self.handle_not_found)()
 
-    def send_error(self, code, message=None, explain=None):
+    def send_error(self, code: int, message=None, *_):
         self.send_response(code, message)
         self.send_header("Content-type", "text/plain")
         self.send_header("Content-Length", "0")
         self.end_headers()
 
-    def shutdown(self):
-        self.server.socket.close()
+    def handle_hello_world(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello World at %s" % time.strftime("%H:%M:%S").encode())
 
-
-def raiser():
-    raise
+    def handle_not_found(self):
+        self.send_error(404)
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, lambda *_: raiser())
     with http.server.HTTPServer(("", PORT), BasicRequestHandler) as httpd:
         shutdown = threading.Thread(target=httpd.shutdown, daemon=True)
         try:
