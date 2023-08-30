7aba88b922431903c8f0e9c8a1badfed80a9fe41
su55y <cesarmilano88@yahoo.com>
2023-08-30 10:24:13 +0300

    crawler base

--- http/crawler/crawler.py
+++ http/crawler/crawler.py
@@ -1,9 +1,76 @@
+import asyncio
+import logging
+from typing import Iterable, Optional, Set
+
 from httpx import AsyncClient
+
+from parser import UrlParser
 
 
 class Crawler:
-    def __init__(self, client: AsyncClient) -> None:
-        ...
+    def __init__(
+        self,
+        client: AsyncClient,
+        urls: Iterable[str],
+        allowed_domains: Optional[Set[str]] = None,
+    ) -> None:
+        self.log = logging.getLogger()
+        self.client = client
+        self.start_urls = set(urls)
+        self.allowed_domains = set(urls)
+        if allowed_domains:
+            self.allowed_domains.update(allowed_domains)
+        self.queue = asyncio.Queue()
+        self.seen = set()
+        self.done = set()
+        self.total = 0
 
     async def run(self) -> None:
-        ...
+        self.log.info("run...")
+
+    async def worker(self):
+        while True:
+            try:
+                await self.process_one()
+            except asyncio.CancelledError:
+                return
+
+    async def process_one(self):
+        url = await self.queue.get()
+        try:
+            await self.crawl(url)
+        except Exception as e:
+            self.log.error(e)
+        finally:
+            self.queue.task_done()
+
+    async def crawl(self, url: str):
+        await asyncio.sleep(1)
+
+        resp = await self.client.get(url, follow_redirects=True)
+        self.log.debug("%s %s" % (resp.status_code, resp.reason_phrase))
+
+        found_links = await self.parse_links(
+            base=str(resp.url),
+            text=resp.text,
+        )
+
+        await self.on_found_links(found_links)
+
+        self.done.add(url)
+
+    async def parse_links(self, base: str, text: str) -> Set[str]:
+        parser = UrlParser(base, self.allowed_domains)
+        parser.feed(text)
+        return parser.found_links
+
+    async def on_found_links(self, urls: Set[str]):
+        self.seen.update(new := urls - self.seen)
+        for url in new:
+            await self.update_queue(url)
+
+    async def update_queue(self, url: str):
+        if self.total >= 8:
+            return
+        self.total += 1
+        await self.queue.put(url)
--- http/crawler/main.py
+++ http/crawler/main.py
@@ -1,10 +1,11 @@
+import argparse
 import asyncio
 import logging
 from pathlib import Path
 
 from httpx import AsyncClient
 
-from .crawler import Crawler
+from crawler import Crawler
 
 LOG_FMT = "[%(asctime)s %(levelname)s] %(message)s (%(funcName)s:%(lineno)d)"
 
@@ -17,11 +18,16 @@ def init_logger(file: Path = Path(__file__).parent.joinpath("crawler.log")) -> N
     logger.addHandler(fh)
 
 
-async def run_crawler():
+def parse_args() -> argparse.Namespace:
+    ...
+
+
+async def run_crawler() -> None:
     async with AsyncClient() as client:
-        crawler = Crawler(client)
+        crawler = Crawler(client, [])
         await crawler.run()
 
 
 if __name__ == "__main__":
+    init_logger()
     asyncio.run(run_crawler())
