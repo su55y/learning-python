152195b6c09e8bd5b1505a91a5f44580ae50c307
su55y <cesarmilano88@yahoo.com>
2023-09-02 15:35:48 +0300

    cleanup

--- playground/entry_point/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="greeter",
-    version="1.0",
-    packages=["greeter"],
-    entry_points={"console_scripts": ["greeter = greeter.run:main"]},
-)
--- playground/typed_mysql_storage/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tb_countries (
-    id int NOT NULL AUTO_INCREMENT,
-    country varchar(255) NOT NULL,
-    capital varchar(255) NOT NULL,
-    PRIMARY KEY (id)
-);
