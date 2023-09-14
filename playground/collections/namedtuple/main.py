33adf125d1bc78937f3325c7c92d23a172db427c
su55y <cesarmilano88@yahoo.com>
2023-09-14 22:04:53 +0300

    to variables

--- playground/collections/namedtuple/main.py
+++ playground/collections/namedtuple/main.py
@@ -9,13 +9,15 @@ def test(conn: sqlite3.Connection):
     cur.execute(
         "CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
     )
-    cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("John Doe", 33))
+    cur.execute(
+        "INSERT INTO users (name, age) VALUES (?, ?)", (name := "John Doe", age := 33)
+    )
     row = cur.execute("select * from users where id = 1 LIMIT 1").fetchone()
 
     u1 = User._make(row)
     print("name: %r, age: %r" % (u1.name, u1.age))
-    assert u1.name == "John Doe"
-    assert u1.age == 33
+    assert u1.name == name
+    assert u1.age == age
 
 
 if __name__ == "__main__":
