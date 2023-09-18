289a6db6639322e19301f8e8a011b954ad9bc5d8
su55y <cesarmilano88@yahoo.com>
2023-09-18 22:45:23 +0300

    implementation

--- playground/protocol/storage/db/sqlite.py
+++ playground/protocol/storage/db/sqlite.py
@@ -1,12 +1,32 @@
+from contextlib import contextmanager
+import sqlite3
+
 from .interface import Rows
 
 
 class SQLite:
+    def __init__(self, connstr: str) -> None:
+        self.connstr = connstr
+
+    @contextmanager
+    def _get_cursor(self):
+        conn = sqlite3.connect(self.connstr)
+        try:
+            cur = conn.cursor()
+            yield cur
+        except:
+            ...
+        finally:
+            conn.close()
+
     def select(self, query: str) -> Rows:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query).fetchall()
 
     def insert(self, query: str, rows: Rows) -> int:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query, rows).rowcount
 
     def delete(self, query: str) -> int:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query).rowcount
