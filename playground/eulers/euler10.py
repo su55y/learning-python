f61a24ce7b3817d8dccc419358d440285eee637c
su55y <cesarmilano88@yahoo.com>
2023-09-19 07:26:01 +0300

    rm useless links

--- playground/eulers/euler10.py
+++ playground/eulers/euler10.py
@@ -2,7 +2,6 @@ print(
     """
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
-(https:#projecteuler.net/problem=10)
 
 expected result: 142913828922
 """
