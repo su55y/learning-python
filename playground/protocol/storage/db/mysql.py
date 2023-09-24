cb02b48c48da74f4cd1ff0d96d97adf43ec459a3
su55y <cesarmilano88@yahoo.com>
2023-09-24 15:51:04 +0300

    mysql

--- playground/protocol/storage/db/mysql.py
+++ playground/protocol/storage/db/mysql.py
@@ -1,12 +1,33 @@
+from contextlib import contextmanager
+
+from mysql.connector import connect
+
 from .interface import Rows
 
 
 class MySQL:
+    def __init__(self, **kwargs) -> None:
+        self.__conf = kwargs
+
+    @contextmanager
+    def _get_cursor(self):
+        conn = connect(**self.__conf)
+        try:
+            with conn.cursor() as cur:
+                yield cur
+        except:
+            ...
+        finally:
+            conn.close()
+
     def select(self, query: str) -> Rows:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query).fetchall()
 
     def insert(self, query: str, rows: Rows) -> int:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query, rows).rowcount
 
     def delete(self, query: str) -> int:
-        ...
+        with self._get_cursor() as cur:
+            return cur.execute(query).rowcount
