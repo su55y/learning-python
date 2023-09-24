99996ef18ba713bd1203dce005a5465d18d369a8
su55y <cesarmilano88@yahoo.com>
2023-09-24 15:53:47 +0300

    mv logging

--- playground/mysql/main.py
+++ playground/mysql/main.py
@@ -1,21 +1,17 @@
 from config import db_config
 from db import MyDB, init_db
-from logging import INFO, basicConfig, error, info
-
-
-def die(e: str):
-    error(e)
-    exit(1)
+import logging
 
 
 def main():
-    basicConfig(
-        level=INFO,
+    logging.basicConfig(
+        level=logging.INFO,
         format="\x1b[38;5;44m%(asctime)s [%(levelname)s]:\x1b[0m %(message)s",
         datefmt="%H:%M:%S %d/%m/%y",
     )
     if err := init_db(**db_config):
-        die(f"DB_CREATE_ERROR: {repr(err)}")
+        logging.critical(f"DB_CREATE_ERROR: {repr(err)}")
+        exit(1)
 
     my_db = MyDB(**db_config)
     tb_countries = "tb_countries"
@@ -34,14 +30,16 @@ def main():
             ]
         ),
     ):
-        die(f"CREATE TABLE ERROR: {repr(err)}")
+        logging.critical(f"CREATE TABLE ERROR: {repr(err)}")
+        exit(1)
 
     if err := my_db.insert_row(
         tb_name=tb_countries,
         cols=[col_country, col_capital],
         vals=("Ukraine", "Kyiv"),
     ):
-        die(f"INSERT_ROW_ERROR: {repr(err)}")
+        logging.critical(f"INSERT_ROW_ERROR: {repr(err)}")
+        exit(1)
 
     insert_count, insert_err = my_db.insert(
         tb_name=tb_countries,
@@ -49,13 +47,13 @@ def main():
         vals=[("United Kingdom", "London"), ("Poland", "Warsaw"), ("India", "Delhi")],
     )
     if insert_err:
-        error(f"INSERT_ERROR: {repr(insert_err)}")
+        logging.error(f"INSERT_ERROR: {repr(insert_err)}")
     else:
-        info(f"{insert_count} rows inserted")
+        logging.info(f"{insert_count} rows inserted")
 
     countries, select_err = my_db.select(tb_name=tb_countries)
     if select_err:
-        error(f"SELECT_ERROR: {repr(select_err)}")
+        logging.error(f"SELECT_ERROR: {repr(select_err)}")
     else:
         for c in countries:
             print(c)
@@ -67,9 +65,9 @@ def main():
         where=[col_country],
     )
     if update_error:
-        error(f"UPDATE_ERROR: {repr(update_error)}")
+        logging.error(f"UPDATE_ERROR: {repr(update_error)}")
     else:
-        info(f"{update_count} rows updated")
+        logging.info(f"{update_count} rows updated")
 
     country, select_row_err = my_db.select(
         tb_name=tb_countries,
@@ -77,7 +75,7 @@ def main():
         where=f"{col_country} = 'India'",
     )
     if select_row_err:
-        error(f"SELECT_ROW_ERROR: {repr(select_row_err)}")
+        logging.error(f"SELECT_ROW_ERROR: {repr(select_row_err)}")
     else:
         for c in country:
             print(c)
@@ -86,13 +84,13 @@ def main():
         tb_name=tb_countries, where=[col_country], vals=["India"]
     )
     if delete_error:
-        error(f"DELETE_ERROR {repr(delete_error)}")
+        logging.error(f"DELETE_ERROR {repr(delete_error)}")
     else:
-        info(f"{delete_count} rows deleted")
+        logging.info(f"{delete_count} rows deleted")
 
     countries2, select_err2 = my_db.select(tb_name=tb_countries)
     if select_err2:
-        error(f"SELECT_ERROR: {repr(select_err)}")
+        logging.error(f"SELECT_ERROR: {repr(select_err)}")
     else:
         for c in countries2:
             print(c)
