467a0720bd1615aa322d0d75bbac077886b1025c
su55y <cesarmilano88@yahoo.com>
2023-09-26 21:32:10 +0300

    reduced

--- playground/eulers/euler20.py
+++ playground/eulers/euler20.py
@@ -1,19 +1,15 @@
+from functools import reduce
+
 print(
     """n! means n * (n - 1) * ... * 3 * 2 * 1.
 For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!.
 
-expected result: 648
-"""
+expected result: 648"""
 )
 
+s = str(reduce(lambda p, n: p * n, range(1, 101)))
+result = reduce(lambda p, n: p + int(s[n]), range(len(s)), 0)
 
-n = 1
-for i in range(2, 101):
-    n *= i
-result = 0
-for i in range(len(str(n))):
-    result += int(str(n)[i])
-
-print("result", result)
+print("result:", result)
