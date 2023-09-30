fd502b03502e1fc5d1e9bd0dcd781aaf0f1a7452
su55y <cesarmilano88@yahoo.com>
2023-09-30 09:08:45 +0300

    refactoring

--- playground/subprocess/scripts/ram_hogs.py
+++ playground/subprocess/scripts/ram_hogs.py
@@ -1,26 +1,10 @@
 import subprocess as sp
 from sys import argv
-from typing import Dict, List, Tuple
 
-COUNT = 10
+LIMIT = 10
 CMD = "ps axch -o cmd,%mem --sort=-%mem"
-HELP = """usage: ram_hogs [-h] [LINES_COUNT]
-Shows the top ram hogs using ps command. Default LINES_COUNT is 10"""
-
-
-def get_ps_output() -> List[str]:
-    if out := sp.getoutput(CMD):
-        return out.splitlines()
-    exit("can't get output from ps")
-
-
-def parse_output(out: List[str]) -> List[Tuple[str, float]]:
-    procs: Dict[str, float] = {}
-    for line in out:
-        name, *_, perc = line.split()
-        procs[name] = procs[name] + float(perc) if procs.get(name) else float(perc)
-    return sorted(procs.items(), key=lambda x: x[-1], reverse=True)
-
+HELP = """usage: ram_hogs [-h] [LIMIT]
+Shows the top ram hogs using ps command. Default LIMIT is 10"""
 
 if __name__ == "__main__":
     args = argv[1:]
@@ -29,13 +13,16 @@ if __name__ == "__main__":
         exit(0)
     if args:
         try:
-            COUNT = int(argv[1])
+            LIMIT = int(argv[1])
         except:
             pass
 
-    print(
-        "\n".join(
-            f"{name}: {perc:.1f}%"
-            for name, perc in parse_output(get_ps_output())[:COUNT]
-        )
-    )
+    procs = {}
+    for line in sp.getoutput(CMD).splitlines():
+        name, *_, perc = line.split()
+        procs[name] = procs[name] + float(perc) if procs.get(name) else float(perc)
+
+    sorted_procs = sorted(procs.items(), key=lambda x: x[-1], reverse=True)[:LIMIT]
+    max_width = max(len(name) for name, _ in sorted_procs) + 1
+    for name, perc in sorted_procs:
+        print(f"{name:{max_width}}", f"{perc:.1f}%".rjust(5))
