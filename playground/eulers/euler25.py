6fd1591f9cfe1ffeb6c760469ecd78395728e20c
su55y <cesarmilano88@yahoo.com>
2023-11-06 22:11:17 +0200

    #25

--- playground/eulers/euler25.py
+++ playground/eulers/euler25.py
@@ -1,22 +1,32 @@
 print(
-    """A permutation is an ordered arrangement of objects. For example, 3124 is
-one possible permutation of the digits 1, 2, 3 and 4. If all of the
-permutations are listed numerically or alphabetically, we call it lexicographic
-order. The lexicographic permutations of 0, 1 and 2 are:
-    012 021 102 120 201 210
-What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5,
-6, 7, 8 and 9?
+    """The Fibonacci sequence is defined by the recurrence relation:
+    F{n} = F{n - 1} + F{n - 2}, where F{1} = 1 and F{2} = 1.
+Hence the first $12$ terms will be:
+    F{1} = 1
+    F{2} = 1
+    F{3} = 2
+    F{4} = 3
+    F{5} = 5
+    F{6} = 8
+    F{7} = 13
+    F{8} = 21
+    F{9} = 34
+    F{10} = 55
+    F{11} = 89
+    F{12} = 144
+The 12th term, F{12}, is the first term to contain three digits.
+What is the index of the first term in the Fibonacci sequence to contain 1000
+digits?
 
 expected result: 4782
 """
 )
 
-a, b, c, counter = 1, 2, 0, 2
-
+a, b, i = 1, 2, 2
 while len(str(a)) < 1000:
-    c = c -a
+    temp = a * -1
     a += b
-    b = b -c
+    b = temp * -1
+    i+=1
 
-    counter+=1
-print("result:", counter)
+print("result:", i)
