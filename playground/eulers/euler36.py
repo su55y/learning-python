27b6de1742adbd227ed0583598d44e60739bf6f9
su55y <cesarmilano88@yahoo.com>
2023-11-12 09:06:35 +0200

    #36

---
+++ playground/eulers/euler36.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+print(
+    """The decimal number, 585 = 1001001001 (binary), is palindromic in both
+bases.
+Find the sum of all numbers, less than one million, which are palindromic in
+base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include
+leading zeros.)
+
+
+expected result: 872187
+"""
+)
+
+def is_palindromic(n: int) -> bool:
+    if (d := f"{n}") != d[::-1]:
+        return False
+    if (b := f"{n:b}") != b[::-1]:
+        return False
+    return True
+
+print("result:", reduce(lambda p, n: p+n if is_palindromic(n) else p, range(1_000_000), 0))
