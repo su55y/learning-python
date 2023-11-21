143d01d36aa82c0afa680f7323fdaa2d20aabe7a
su55y <cesarmilano88@yahoo.com>
2023-11-21 22:39:41 +0200

    #48

---
+++ playground/eulers/euler48.py
@@ -0,0 +1,11 @@
+from functools import reduce
+
+print(
+    """The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+expected result: 9110846700
+"""
+)
+
+print("result:", str(reduce(lambda p, n: p + n**n,range(1, 1001), 0))[-10:])
