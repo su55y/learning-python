aafcd9bb852a606443a5518d3eebd830aaf1363c
su55y <cesarmilano88@yahoo.com>
2023-11-25 20:21:47 +0200

    #49

---
+++ playground/eulers/euler49.py
@@ -0,0 +1,47 @@
+print(
+    """The arithmetic sequence, 1487, 4817, 8147, in which each of the terms
+increases by 3330, is unusual in two ways: (i) each of the three terms are
+prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+exhibiting this property, but there is one other 4-digit increasing sequence.
+What 12-digit number do you form by concatenating the three terms in this
+sequence?
+
+expected result: 296962999629
+"""
+)
+
+def is_perm(x: int, y: int) -> bool:
+    s1, s2 = str(x), str(y)
+    for i in range(len(s1)):
+        if s2.find(s1[i:i+1]) >= 0 or s1.find(s2[i:i+1]) >= 0:
+            return False
+    return True
+
+LIMIT = 10_000
+arr = [False for _ in range(LIMIT)]
+prime = 2
+while True:
+    for i in range(2*prime, LIMIT, prime):
+        arr[i] = True
+    i = prime+1
+    while i < LIMIT and arr[i]:
+        i+=1
+    if i < LIMIT:
+        prime = i
+    else:
+        break
+
+for i in range(1000, LIMIT):
+    for j in range(i+1, LIMIT):
+        if (
+            i != 1487 and
+            not arr[i] and
+            not arr[j] and
+            is_perm(i, j) and
+            2*j-i < LIMIT and
+            not arr[2*j-i] and
+            is_perm(i, 2*j-i)
+        ):
+            print("result:","%d%d%d" % (i, j, 2*j-i))
+            exit(0)
