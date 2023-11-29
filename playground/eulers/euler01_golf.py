8cd6626bc93607d7d4b5d217c538735bfeeeb331
su55y <cesarmilano88@yahoo.com>
2023-11-29 20:43:26 +0200

    rm spaces

--- playground/eulers/euler01_golf.py
+++ playground/eulers/euler01_golf.py
@@ -1,1 +1,1 @@
-print(__import__('functools').reduce(lambda p, n: p + n if n % 3 == 0 or n % 5 == 0 else p, range(1000)),)
+print(__import__('functools').reduce(lambda p,n: p+n if n%3==0 or n%5==0 else p,range(1000)))
