7a96d5e41da2c2527cf5c6f6b845a657ad43e6d2
su55y <cesarmilano88@yahoo.com>
2023-12-02 20:09:18 +0200

    #04 simplified

--- playground/eulers/euler04.py
+++ playground/eulers/euler04.py
@@ -8,18 +8,13 @@ expected result: 906609
 )
 
 
-def result() -> int:
-    i = j = i_stop = j_stop = 1000
-    while (i := i - 1) > 100:
-        while (j := j - 1) > 100:
-            if str(r := i * j) == str(r)[::-1]:
-                return r
-            if j % 10 == 0:
-                break
+i = j = i_stop = j_stop = 1000
+while (i := i - 1) > 100:
+    while (j := j - 1) > 100:
+        if str(r := i * j) == str(r)[::-1]:
+            print("result:", r)
+            exit(0)
+        if j % 10 == 0:
+            break
 
-        j = j_stop = i_stop if i_stop % 10 == 0 else j_stop - 10
-
-    return 0
-
-
-print("result:", result())
+    j = j_stop = i_stop if i_stop % 10 == 0 else j_stop - 10
