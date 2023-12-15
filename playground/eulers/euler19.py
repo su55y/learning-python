84f8e40469395bd0d7e905f0b1810e12b5f3c5d4
su55y <cesarmilano88@yahoo.com>
2023-12-15 20:28:34 +0200

    #19 fmt

--- playground/eulers/euler19.py
+++ playground/eulers/euler19.py
@@ -12,7 +12,7 @@ research for yourself.
   * A leap year occurs on any year evenly divisible by 4, but not on a century
 unless it is divisible by 400.
 
-How many Sundays fell on the first of the month during the twentieth
+How many sundays fell on the first of the month during the twentieth
 century (1 Jan 1901 to 31 Dec 2000)?
 
 expected result: 171
@@ -24,12 +24,12 @@ months = [0 for _ in range(12)]
 for i in range(11):
     months[i + 1] = (months[i] + days_in_months[i]) % 7
 
-sundays = 0
+res = 0
 for y in range(1900, 2000):
     for i in range(len(months)):
         c = 366 if (y % 4 == 0 and i <= 1) or ((y + 1) % 4 == 0 and i > 1) else 365
         if (d := (months[i] + c) % 7) == 6:
-            sundays += 1
+            res += 1
         months[i] = d
 
-print("result:", sundays)
+print("result:", res)
