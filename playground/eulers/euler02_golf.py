16fe0e174057d8577719c6504ddec90223f34faa
su55y <cesarmilano88@yahoo.com>
2024-01-22 00:08:37 +0200

    \#2 golf: switch to sum

--- playground/eulers/euler02_golf.py
+++ playground/eulers/euler02_golf.py
@@ -2,8 +2,7 @@ import itertools as t
 c={0:0,1:1}
 L=4000000
 def f(n):
-    if n in c:
-        return c[n]
+    if n in c:return c[n]
     c[n]=f(n-1)+f(n-2)
     return c[n]
-print(__import__('functools').reduce(lambda p,n:p+n if n%2==0 else p,[c[i] for i in t.takewhile(lambda x:f(x)<L,t.count())]))
+print(sum(n for n in(c[i]for i in t.takewhile(lambda x:f(x)<L,t.count()))if n%2==0))
