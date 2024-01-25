83723d28484925153b910d69e91156c87858d223
su55y <cesarmilano88@yahoo.com>
2024-01-26 00:19:58 +0200

    basic list

--- playground/curses/list.py
+++ playground/curses/list.py
@@ -6,23 +6,31 @@ from typing import Sequence
 class LinesPrinter:
     def __init__(self, lines: Sequence[str]) -> None:
         self.lines = lines
+        self.active_index = 0 if len(self.lines) else -1
 
     def run(self, stdscr: "curses._CursesWindow") -> None:
         curses.curs_set(0)
         self.s = stdscr
         while 1:
-            self.print_lines()
+            try:
+                self.print_lines()
+            except:
+                pass
             match chr(_ := self.s.getch()):
                 case "q":
                     break
+                case "j":
+                    self.active_index = (self.active_index + 1) % len(self.lines)
+                case "k":
+                    self.active_index = (self.active_index - 1) % len(self.lines)
 
     def print_lines(self) -> None:
         self.s.clear()
+        curses.update_lines_cols()
         self.s.addstr("[%s] %d lines\n" % (time.strftime("%T"), curses.LINES))
-        for line in self.lines[: curses.LINES - 1]:
-            self.s.addstr("%s\n" % line)
-        self.s.refresh()
+        for i, line in enumerate(self.lines[: curses.LINES - 1]):
+            self.s.addstr("%s %s\n" % (">" if i == self.active_index else " ", line))
 
 
 if __name__ == "__main__":
-    curses.wrapper(LinesPrinter(("aaa", "bbb", "ccc")).run)
+    curses.wrapper(LinesPrinter(tuple(chr(i) * 3 for i in range(97, 100))).run)
