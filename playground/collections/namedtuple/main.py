dd1f0c0b20619981517cf49402a77ecd2bc0a28e
su55y <cesarmilano88@yahoo.com>
2024-01-31 00:11:54 +0200

    namedtuple fmt

--- playground/collections/namedtuple/main.py
+++ playground/collections/namedtuple/main.py
@@ -3,21 +3,28 @@ import sqlite3
 
 User = namedtuple("User", "id, name, age")
 
+CREATE_TABLE = """
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+)"""
+
 
 def test(conn: sqlite3.Connection):
     cur = conn.cursor()
-    cur.execute(
-        "CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)"
-    )
-    cur.execute(
-        "INSERT INTO users (name, age) VALUES (?, ?)", (name := "John Doe", age := 33)
-    )
-    row = cur.execute("select * from users where id = 1 LIMIT 1").fetchone()
+    cur.execute(CREATE_TABLE)
 
-    u1 = User._make(row)
-    print("name: %r, age: %r" % (u1.name, u1.age))
-    assert u1.name == name
-    assert u1.age == age
+    name, age = "John Doe", 30
+    cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))
+    assert cur.rowcount == 1, f"{cur.rowcount=}"
+
+    row = cur.execute("SELECT * FROM users WHERE id = 1 LIMIT 1").fetchone()
+
+    user = User._make(row)
+    print("name: %r, age: %r" % (user.name, user.age))
+    assert user.name == name, f"{user.name=}"
+    assert user.age == age, f"{user.age=}"
 
 
 if __name__ == "__main__":
@@ -25,6 +32,6 @@ if __name__ == "__main__":
     try:
         test(conn)
     except Exception as e:
-        print(repr(e))
+        print("SQL Error: %s" % e)
     finally:
         conn.close()
