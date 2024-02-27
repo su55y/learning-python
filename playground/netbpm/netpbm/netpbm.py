dbf812e348a42776cc6c4dcbc5d4da67a53def72
su55y <cesarmilano88@yahoo.com>
2024-02-27 23:44:59 +0200

    netpbm: binary test

--- playground/netbpm/netpbm/netpbm.py
+++ playground/netbpm/netpbm/netpbm.py
@@ -1,13 +1,14 @@
 from collections.abc import Generator, Sequence
 from enum import IntEnum
-from io import TextIOWrapper
-from typing import Optional, Tuple
+from typing import IO, Any, Optional, Tuple
 
 
 class MagicNumber(IntEnum):
     P1 = 1
     P2 = 2
     P3 = 3
+    P4 = 4
+    P6 = 6
 
 
 class Netpbm:
@@ -26,31 +27,53 @@ class Netpbm:
         self.max_value = max_value
 
     def write(self) -> Optional[Exception]:
+        mode = "w" if self.magic_number < 4 else "wb"
         try:
-            with open(self.file, "w") as f:
-                self._write_header(f)
+            with open(self.file, mode) as f:
+                if mode == "w":
+                    self._write_header(f)
+                else:
+                    self._write_header_bin(f)
                 self._write_data(f)
         except Exception as e:
             return e
 
-    def _write_header(self, file: TextIOWrapper) -> None:
+    def _write_header(self, file: IO[Any]) -> None:
         file.write("P%d\n" % self.magic_number)
         file.write("%d %d\n" % self.dimensions)
         match self.magic_number:
             case MagicNumber.P2 | MagicNumber.P3:
                 file.write("%d\n" % self.max_value)
 
-    def _write_data(self, file: TextIOWrapper):
+    def _write_header_bin(self, file: IO[Any]) -> None:
+        file.write(("P%d\n" % self.magic_number).encode("ascii"))
+        file.write(("%d %d\n" % self.dimensions).encode("ascii"))
+        match self.magic_number:
+            case MagicNumber.P2 | MagicNumber.P3:
+                file.write(("%d\n" % self.max_value).encode("ascii"))
+
+    def _write_data(self, file: IO[Any]):
         match self.magic_number:
             case MagicNumber.P1 | MagicNumber.P2:
                 self._write_matrix(file)
             case MagicNumber.P3:
                 self._write_rgb_sequence(file)
+            case MagicNumber.P4:
+                self._write_matrix_bin(file)
+            case MagicNumber.P6:
+                self._write_rgb_sequence_bin(file)
 
-    def _write_matrix(self, file: TextIOWrapper):
+    def _write_matrix(self, file: IO[Any]):
         for row in self.data:
             file.write("%s\n" % " ".join(str(n) for n in row))
 
-    def _write_rgb_sequence(self, file: TextIOWrapper):
+    def _write_rgb_sequence(self, file: IO[Any]):
         for color in self.data:
             file.write("%d %d %d\n" % color)
+
+    def _write_rgb_sequence_bin(self, file: IO[Any]):
+        for color in self.data:
+            file.write(color)
+
+    def _write_matrix_bin(self, file: IO[Any]):
+        file.write(self.data)
--- playground/netbpm/tests.py
+++ playground/netbpm/tests.py
@@ -7,7 +7,7 @@ from netpbm import Netpbm, MagicNumber
 class TestNetpbm(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.dimensions = 1920, 1080
+        cls.dimensions = 80, 80
 
     def test_pbm(self):
         colors = (0x00, 0x01)
@@ -30,4 +30,18 @@ class TestNetpbm(unittest.TestCase):
         w, h = self.dimensions
         data = (random.choice(colors) for _ in range(w * h))
         err = Netpbm(MagicNumber.P3, "/tmp/test.ppm", data, (w, h), max_value).write()
+        self.assertIsNone(err)
+
+    def test_pbm_bin(self):
+        colors = (0x00, 0x01)
+        w, h = self.dimensions
+        image = tuple(random.choice(colors) for _ in range(w * h))
+        byte_groups = (image[i : i + 8] for i in range(0, len(image), 8))
+        data = bytearray()
+        for group in byte_groups:
+            byte = 0
+            for i, bit in enumerate(group):
+                byte |= bit << (7 - i)
+            data.append(byte)
+        err = Netpbm(MagicNumber.P4, "/tmp/testb.pbm", data, (w, h)).write()
         self.assertIsNone(err)
