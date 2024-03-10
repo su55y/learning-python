38620e40575f32ba2a2134d99086da312423149d
su55y <cesarmilano88@yahoo.com>
2024-03-10 15:19:29 +0200

    #43g fmt

--- playground/eulers/euler43_golf.py
+++ playground/eulers/euler43_golf.py
@@ -1,11 +1,7 @@
 r=0
 def f(p,s):
     global r
-    if not len(s):
-        if p[0]=="0":return
-        for i in range(1,8):
-            if int(p[i:i+3])%[2,3,5,7,11,13,17][i-1]:return
-        r+=int(p)
+    if len(s)==0and p[0]!="0"and all(int(p[i+1:i+4])%d==0 for i,d in enumerate((2,3,5,7,11,13,17))):r+=int(p)
     else:
         for i in range(len(s)):f(p+s[i:i+1],s[:i]+s[i+1:])
 print(f("","0123456789")or r)
