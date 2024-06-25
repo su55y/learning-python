0f6524e3cf9f8a2d3a1dba8bf72b95417337c236
su55y <cesarmilano88@yahoo.com>
2024-06-25 21:48:31 +0300

    playground/curses/pad: mirrored pad content

--- playground/curses/pad.py
+++ playground/curses/pad.py
@@ -9,6 +9,8 @@ class Key(IntEnum):
     g = ord("g")
     G = ord("G")
     q = ord("q")
+    l = ord("l")
+    h = ord("h")
 
 
 def read_self_lines() -> List[str]:
@@ -23,25 +25,35 @@ def main(scr: "curses._CursesWindow"):
     curses.mousemask(-1)
     scr.refresh()
 
-    pad_content = read_self_lines()
+    self_lines = read_self_lines()
+    rev_self_lines = list(map(lambda s: s[::-1].strip(), self_lines))
+    pad_content = self_lines
+    state = 0
+    pad_pos = 0
     pad_height = len(pad_content) + 1
     pad = curses.newpad(pad_height, scr.getmaxyx()[-1])
-    pad_pos = 0
 
-    def add_lines(pad):
+    def redraw_pad(pad):
+        _, w = scr.getmaxyx()
         for i, line in enumerate(pad_content):
-            pad.addstr("%d %s" % (i + 1, line))
+            if state == 1:
+                text = "%s %2d" % (line, i + 1)
+                text = f"{text:>{w}}"
+            else:
+                text = "%2d %s" % (i + 1, line)
+                text = f"{text:<{w}}"
+            pad.addnstr(i, 0, text, min(len(text), w))
 
     def handle_resize(pad):
         pad.clear()
         h, w = scr.getmaxyx()
         pad = curses.newpad(pad_height, w)
-        add_lines(pad)
+        redraw_pad(pad)
         pad.refresh(pad_pos, 0, 0, 0, h - 1, w - 1)
         scr.refresh()
         return pad
 
-    add_lines(pad)
+    redraw_pad(pad)
     while 1:
         h, w = scr.getmaxyx()
         pad.refresh(pad_pos, 0, 0, 0, h - 1, w - 1)
@@ -51,10 +63,20 @@ def main(scr: "curses._CursesWindow"):
                 pad_pos = min(pad_pos + 1, pad.getyx()[0] - h)
             case Key.k | curses.KEY_UP:
                 pad_pos = max(0, pad_pos - 1)
-            case Key.g | curses.KEY_LEFT:
+            case Key.g:
                 pad_pos = 0
-            case Key.G | curses.KEY_RIGHT:
+            case Key.G:
                 pad_pos = pad.getyx()[0] - h
+            case Key.l | curses.KEY_RIGHT:
+                if state == 0:
+                    pad_content = rev_self_lines
+                    state = 1
+                    redraw_pad(pad)
+            case Key.h | curses.KEY_LEFT:
+                if state == 1:
+                    pad_content = self_lines
+                    state = 0
+                    redraw_pad(pad)
             case Key.q:
                 break
             case curses.KEY_RESIZE:
