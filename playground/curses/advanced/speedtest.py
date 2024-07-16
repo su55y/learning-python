8e4b2cf115b7ba6dc25b26aff268ed951875eeee
su55y <cesarmilano88@yahoo.com>
2024-07-16 20:58:13 +0300

    curses: speedtest just print

--- playground/curses/advanced/speedtest.py
+++ playground/curses/advanced/speedtest.py
@@ -19,8 +19,10 @@ def main(stdscr: "curses._CursesWindow"):
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_YELLOW, 0)
     fg_yellow = curses.color_pair(1)
-    game_words = rnd_words()
-    game_words_len = words_len(game_words)
+    raw_game_words = rnd_words()
+    game_words_str = " ".join(raw_game_words)
+    # game_words_len = words_len(game_words)
+    game_words_len = len(game_words_str)
     index = -1
 
     max_y, max_x = stdscr.getmaxyx()
@@ -28,17 +30,26 @@ def main(stdscr: "curses._CursesWindow"):
     def print_words() -> None:
         my, mx = stdscr.getmaxyx()
         y = 0
-        for x, ch in enumerate(game_words):
+        color_pair = 0
+        for x, ch in enumerate(game_words_str):
             if x + 1 == mx:
                 y += 1
-            stdscr.addnstr(y, x, ch, 1)
+            if y + x <= index:
+                color_pair = fg_yellow
+            else:
+                color_pair = 0
+            stdscr.addnstr(y, x, ch, 1, color_pair)
 
+    print_words()
+    stdscr.refresh()
     while True:
         ch = stdscr.getch()
-        max_y, max_x = stdscr.getmaxyx()
-        if max_y < 1:
-            continue
-        stdscr.clrtoeol()
+        if ch == 263:
+            index = max(-1, index - 1)
+        else:
+            index += 1
+        # stdscr.clrtoeol()
+        print_words()
         stdscr.refresh()
 
 
