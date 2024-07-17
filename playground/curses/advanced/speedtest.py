6995a0960b3047f866c47f56525efe2cc4169d0d
su55y <cesarmilano88@yahoo.com>
2024-07-18 00:04:44 +0300

    curses: speedtest functional print

--- playground/curses/advanced/speedtest.py
+++ playground/curses/advanced/speedtest.py
@@ -3,7 +3,7 @@ import curses
 import random
 
 valid_keys = set(map(ord, string.ascii_lowercase))
-words_count = 10
+words_count = 20
 
 LOREM = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
 words = list(
@@ -27,18 +27,44 @@ def main(stdscr: "curses._CursesWindow"):
 
     max_y, max_x = stdscr.getmaxyx()
 
+    def words_by_rows(mx) -> list[list[str]]:
+        rows = [[]]
+        i = 0
+        for word in raw_game_words:
+            l = len("".join(rows[i])) + len(rows[i]) + len(word)
+            if l >= mx:
+                i += 1
+                rows.append([word])
+            else:
+                rows[i].append(word)
+        return rows
+
     def print_words() -> None:
         my, mx = stdscr.getmaxyx()
-        y = 0
         color_pair = 0
-        for x, ch in enumerate(game_words_str):
-            if x + 1 == mx:
-                y += 1
-            if y + x <= index:
-                color_pair = fg_yellow
-            else:
-                color_pair = 0
-            stdscr.addnstr(y, x, ch, 1, color_pair)
+        rows = words_by_rows(mx)
+        char_index = 0
+        for y, row in enumerate(rows):
+            x = 0
+            for _, word in enumerate(row):
+                for i, ch in enumerate(word):
+                    if char_index <= index:
+                        color_pair = fg_yellow
+                    else:
+                        color_pair = 0
+                    try:
+                        stdscr.addnstr(y, x, ch, 1, color_pair)
+                    except:
+                        pass
+                    x += 1
+                    char_index += 1
+                try:
+                    stdscr.addnstr(y, x, " ", 1)
+                except:
+                    pass
+                x += 1
+                char_index += 1
+            stdscr.refresh()
 
     print_words()
     stdscr.refresh()
@@ -46,7 +72,7 @@ def main(stdscr: "curses._CursesWindow"):
         ch = stdscr.getch()
         if ch == 263:
             index = max(-1, index - 1)
-        else:
+        elif ch in valid_keys:
             index += 1
         # stdscr.clrtoeol()
         print_words()
