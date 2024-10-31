4a6e6c53ba8fdcf2c860fce72abbe41945a49361
su55y <cesarmilano88@yahoo.com>
2024-10-31 11:17:31 +0200

    playground/subprocess/notes: notes class

--- playground/subprocess/scripts/notes.py
+++ playground/subprocess/scripts/notes.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 from pathlib import Path
 import re
@@ -6,9 +7,7 @@ import time
 
 
 EDITOR = None
-NOTES_DIR_PATH = Path().home() / ".notes"
-MONTH_DIR = NOTES_DIR_PATH / time.strftime("%Y_%02m")
-TODAY_FILE = MONTH_DIR / time.strftime("%02d")
+DEFAULT_NOTES_DIR_PATH = Path().home() / ".notes"
 FILE_FMT = """
 =============================
 {date}
@@ -20,19 +19,36 @@ FILE_FMT = """
 )
 
 
-def init_files():
-    if not NOTES_DIR_PATH.exists():
-        resp = input(f"Directory {NOTES_DIR_PATH} not exists, create it? [y/n]: ")
-        if not re.match("[yY](?:es)?", resp, flags=re.IGNORECASE):
-            exit(0)
+@dataclass
+class Config:
+    notes_dir: Path
 
-        NOTES_DIR_PATH.mkdir(mode=0o755)
-    if not MONTH_DIR.exists():
-        MONTH_DIR.mkdir(mode=0o755)
 
-    if not TODAY_FILE.exists():
-        with open(TODAY_FILE, "w") as f:
-            f.write(FILE_FMT)
+def default_config() -> Config:
+    return Config(notes_dir=DEFAULT_NOTES_DIR_PATH)
+
+
+class Notes:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.notes_dir = self.config.notes_dir
+        self.month_dir = self.notes_dir / time.strftime("%Y_%02m")
+        self.today_file = self.month_dir / time.strftime("%02d")
+        self._init_files()
+
+    def _init_files(self):
+        if not self.notes_dir.exists():
+            resp = input(f"Directory {self.notes_dir} not exists, create it? [y/n]: ")
+            if not re.match("[yY](?:es)?", resp, flags=re.IGNORECASE):
+                exit(0)
+
+            self.notes_dir.mkdir(mode=0o755)
+        if not self.month_dir.exists():
+            self.month_dir.mkdir(mode=0o755)
+
+        if not self.today_file.exists():
+            with open(self.today_file, "w") as f:
+                f.write(FILE_FMT)
 
 
 def get_last_line(filepath: Path) -> str:
@@ -40,18 +56,19 @@ def get_last_line(filepath: Path) -> str:
 
 
 if __name__ == "__main__":
+    config = default_config()
     EDITOR = os.environ.get("EDITOR", None)
     if EDITOR is None:
         print("EDITOR env is not set")
         exit(1)
 
-    init_files()
+    notes = Notes(config)
 
-    p = sp.run([EDITOR, str(TODAY_FILE), "+"], capture_output=False)
+    p = sp.run([EDITOR, str(notes.today_file), "+"], capture_output=False)
     if p.returncode != 0:
         exit(1)
 
-    last_line = get_last_line(TODAY_FILE)
+    last_line = get_last_line(notes.today_file)
     if last_line != "":
-        with open(TODAY_FILE, "a") as f:
+        with open(notes.today_file, "a") as f:
             f.write("\n\n")
